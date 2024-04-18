238c171fe4361b192c88665fcee6b4e619f4a5e7
Vince489 <vincentcornelius489@gmail.com>
2024-04-18 16:26:13 -0400

    offline

--- sw.js
+++ sw.js
@@ -39,33 +39,39 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('Service Worker: Fetching');
-  event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        if (cachedResponse) {
-          // If a cached response is found, return it
-          console.log('Service Worker: Cached response found');
-          return cachedResponse;
-        }
+  const request = event.request;
+  
+  // Only cache requests with HTTP or HTTPS schemes
+  if (request.url.startsWith('http')) {
+    event.respondWith(
+      caches.match(request)
+        .then(cachedResponse => {
+          if (cachedResponse) {
+            // If a cached response is found, return it
+            console.log('Service Worker: Cached response found');
+            return cachedResponse;
+          }
 
-        // If no cached response is found, fetch from the network
-        console.log('Service Worker: Fetching from network');
-        return fetch(event.request)
-          .then(networkResponse => {
-            // Cache the fetched response
-            const clonedResponse = networkResponse.clone();
-            caches.open(cacheName)
-              .then(cache => {
-                cache.put(event.request, clonedResponse);
-              });
-            return networkResponse;
-          })
-          .catch(() => {
-            // If fetching from the network fails, return a fallback response
-            console.log('Service Worker: Fetch failed; serving offline fallback');
-            return caches.match(OFFLINE_URL);
-          });
-      })
-  );
+          // If no cached response is found, fetch from the network
+          console.log('Service Worker: Fetching from network');
+          return fetch(request)
+            .then(networkResponse => {
+              // Cache the fetched response
+              const clonedResponse = networkResponse.clone();
+              caches.open(cacheName)
+                .then(cache => {
+                  cache.put(request, clonedResponse);
+                });
+              return networkResponse;
+            })
+            .catch(() => {
+              // If fetching from the network fails, return a fallback response
+              console.log('Service Worker: Fetch failed; serving offline fallback');
+              return caches.match(OFFLINE_URL);
+            });
+        })
+    );
+  }
 });
+
 
